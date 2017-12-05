7867573b270413749846e715a4fbe08f676a9d51
treeforever <treeforever7@gmail.com>
2017-12-04 23:07:27 -0500

    read cookie

--- src/services/cart.ts
+++ src/services/cart.ts
@@ -1,36 +1,32 @@
-import FluxCapacitor, { Events, Selectors, Store } from '@storefront/flux-capacitor';
-import { core } from '../core/service';
-import LazyService from '../core/service/lazy';
-import Tag from '../core/tag';
-import { WINDOW } from '../core/utils';
-import StoreFront from '../storefront';
-
+import FluxCapacitor, {
+  Events,
+  Selectors,
+  Store
+} from "@storefront/flux-capacitor";
+import { core } from "../core/service";
+import LazyService from "../core/service/lazy";
+import Tag from "../core/tag";
+import { WINDOW } from "../core/utils";
+import StoreFront from "../storefront";
+import * as Cookie from "js-cookie";
 @core
 class CartService extends LazyService {
-
   lazyInit() {
-    console.log('lazy init')
     this.app.flux.on(Events.CREATE_CART, this.createCart);
   }
 
   createCart = () => {
-    //get visitorId and sessionId from tracker
-    console.log('creating')
-    this.readCookie();
-    
-    const visitorId = 'visitorId';
-    const sessionId = 'sessionId';
+    const { visitorId, sessionId } = this.readCookie();
+
     this.app.flux.createCart(visitorId, sessionId);
-  }
+  };
 
   readCookie = () => {
-    const cookies = new Window().document.cookie;
-    const sessionId = cookies.slice(14, 39);
-    const visitorId = cookies.slice(55);
-    
-    console.log('visitor', visitorId, sessionId);
-  }
-
+    const cookies = WINDOW().document.cookie;
+    const sessionId = Cookie.get("gbi_sessionId");
+    const visitorId = Cookie.get("gbi_visitorId");
+    return { visitorId, sessionId }
+  };
 }
 
 export default CartService;
