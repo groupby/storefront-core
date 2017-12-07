47352c828a0cc38d6bee218195ca06fb83defb99
Mengqi <mengqi.zhu@groupbyinc.com>
2017-12-07 17:26:02 -0500

    wip: rehydrate

--- src/services/cart.ts
+++ src/services/cart.ts
@@ -10,28 +10,18 @@ import StoreFront from '../storefront';
 class CartService extends LazyService {
   lazyInit() {
     this.app.flux.on(Events.CREATE_CART, this.createCart);
-    this.app.flux.on(Events.CART_ID_UPDATED, this.writeToCookie);
   }
 
   createCart = () => {
+    console.log('create');
     const { visitorId, sessionId } = this.readCookie();
 
     this.app.flux.createCart(visitorId, sessionId);
   }
 
-  writeToCookie = (cartId: string) => {
-    Cookie.set('gb_cartId', cartId);
-  }
-
   readCookie = () => {
-    const cookies = WINDOW().document.cookie;
-    // if (this in cookies)
 
-    // need to get cart
-    const { cart } = this.app.flux.store.getState().data.present;
-    // const { cartId } = this.app.flux.selectors.cart;
-    const sessionId = Cookie.get('gbi_sessionId');
-    const visitorId = Cookie.get('gbi_visitorId');
+    const  { visitorId, sessionId }  = this.app.flux.store.getState().data.present.cart;
     return { visitorId, sessionId };
   }
 
