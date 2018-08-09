5ff0d4a0624add2ca1e4704f5d3fa579b9528996
Johann Tutor <johann.tutor@groupbyinc.com>
2018-08-09 11:09:24 -0400

    Test for missing handlers on each phase

--- test/unit/core/tag/mixins/sugar.ts
+++ test/unit/core/tag/mixins/sugar.ts
@@ -4,10 +4,32 @@ import { camelCase } from '../../../../../src/core/utils';
 import suite from '../../../_suite';
 
 suite('Sugar Mixin', ({ expect, spy, sinon }) => {
+  function testMissingHandler(phase: string) {
+    it(`should not error if the ${phase} handler does not exist on the tag`, () => {
+      let handler;
+      const assignHandler = (p, h) => {
+        if (p === phase) {
+          handler = h;
+        }
+      };
+      const tag: any = {
+        on: assignHandler,
+        one: assignHandler,
+        sugarMixin,
+      };
+
+      tag.sugarMixin();
+
+      expect(handler).not.to.throw;
+    });
+  }
+
   describe('sugarMixin()', () => {
     SUGAR_EVENTS.filter(
       (phase) => phase !== Phase.UPDATE && phase !== Phase.UPDATED
     ).forEach((phase) => {
+      testMissingHandler(phase);
+
       it(`should listen once for ${phase}`, () => {
         const handlers = {};
         const one = spy((p, h) => { handlers[p] = h; });
@@ -30,6 +52,8 @@ suite('Sugar Mixin', ({ expect, spy, sinon }) => {
     });
 
     [Phase.UPDATE, Phase.UPDATED].forEach((phase) => {
+      testMissingHandler(phase);
+
       it(`should listen on ${phase} and pass the previous props and state`, () => {
         const handlers = {};
         const on = spy((p, h) => { handlers[p] = h; });
@@ -59,25 +83,6 @@ suite('Sugar Mixin', ({ expect, spy, sinon }) => {
         handlers[phase](arg);
         expect(eventListener).to.be.calledWith(nextProps, nextState, arg);
       });
-    });
-
-    it('should not error if the function does not exist on the tag', () => {
-      const handlers: any = {};
-      const on = spy();
-      const tag: any = {
-        on: (p, h) => { handlers[p] = h; },
-        one: (p, h) => { handlers[p] = h; },
-        onMount: true,
-        onUpdated: null,
-        sugarMixin,
-      };
-
-      tag.sugarMixin();
-
-      expect(() => handlers.onBeforeMount()).not.to.throw;
-      expect(() => handlers.onMount()).not.to.throw;
-      expect(() => handlers.onUpdate()).not.to.throw;
-      expect(() => handlers.onUpdated()).not.to.throw;
     });
   });
 });
